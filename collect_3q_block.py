d3bc6f1682eec4aea1848d0cffb166824c72afd2
Yu-Cheng Chung <tearofclown0428@gmail.com>
2024-08-20 21:08:58 +0800

    something

--- collect_3q_block.py
+++ collect_3q_block.py
@@ -16,12 +16,15 @@ from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 from qiskit_ibm_runtime.fake_provider import FakeKyoto, FakeNairobi
 from qiskit.circuit.random import random_circuit
 from qiskit.transpiler import PassManager
-from qiskit.transpiler.passes import Optimize1qGates
+from qiskit.transpiler.passes import Optimize1qGates, CommutationAnalysis,CommutativeCancellation
 from qiskit.transpiler.passes.optimization.consolidate_blocks import ConsolidateBlocks
-
-
-
-
+from qiskit import QuantumCircuit
+import pyscf
+import pyscf.cc
+import pyscf.data.elements
+import ffsim
+from qiskit_aer import QasmSimulator
+import qiskit
 
 class Collect_3q_blocks(AnalysisPass):
     def run(self, dag: DAGCircuit) -> DAGCircuit:
@@ -75,14 +78,15 @@ class Collect_3q_blocks(AnalysisPass):
                 group_qubit = [i for i in q_args]
         groups.append(group)
         three_q_blocks =[]
-        # pack each group into a block
-        # print(groups)
         for group in groups:
             three_q_block = []
             for index in group:
                 assert type(two_q_blocks[index]) == list
+
                 for node in two_q_blocks[index]:
                     three_q_block.append(node)
+                
+            # if len(two_q_blocks[index]) >20:
             three_q_blocks.append(three_q_block)
         
         return three_q_blocks, centers
@@ -96,7 +100,7 @@ class MLUnitarySynthesis(TransformationPass):
         # print(self.property_set["commutation_set"])
         centers = self.property_set["centers"]
         nodes = dag.op_nodes()
-        index = len(centers)-1
+        # index = len(centers)-1
         for node in dag.op_nodes():
             if node.op.name =="unitary":
                 # print(index)
@@ -122,6 +126,9 @@ class MLUnitarySynthesis(TransformationPass):
                         break
                 # print(node.qargs)
                 matrix = node.matrix
+                if matrix.shape != (8, 8):
+                    print(matrix.shape)
+                    continue
                 qc = QuantumCircuit(3)
                 qc_target = QuantumCircuit(3)
                 # print(matrix)
@@ -151,59 +158,109 @@ class MLUnitarySynthesis(TransformationPass):
                 def cost_func(params):
                     qc_test = qc.assign_parameters(params)
                     state_vector = Statevector(qc_test)
-                    fidelity = state_vector.inner(state_vector_target)
+                    fidelity = abs(state_vector.inner(state_vector_target))
                     return 1- fidelity
                 
                 optimizer = COBYLA(maxiter=500)
                 initial_point = np.random.rand(p_index)*2*np.pi
                 res = optimizer.minimize(cost_func, initial_point)
+
                 qc = qc.assign_parameters(res.x)
 
                 minidag = DAGCircuit()
                 Register = QuantumRegister(3)
                 minidag.add_qreg(Register)
                 minidag.apply_operation_back(qc.to_instruction(), [Register[i] for i in range(3)])
-                index -=1
+                # index -=1
                 dag.substitute_node_with_dag(node, minidag)
             else :
                 pass
         return dag
 
-from qiskit import QuantumCircuit
 
 
-# qc = ZZFeatureMap(5,2)
-qc = random_circuit(5,20)
+# Build N2 molecule
+mol = pyscf.gto.Mole()
+mol.build(
+    atom=[["N", (0, 0, 0)], ["N", (1.0, 0, 0)]],
+    basis="6-31g",
+    symmetry="Dooh",
+)
+
+# Define active space
+n_frozen = 4
+active_space = range(n_frozen, mol.nao_nr())
+
+# Get molecular data and Hamiltonian
+scf = pyscf.scf.RHF(mol).run()
+mol_data = ffsim.MolecularData.from_scf(scf, active_space=active_space)
+norb, nelec = mol_data.norb, mol_data.nelec
+mol_hamiltonian = mol_data.hamiltonian
+print(f"norb = {norb}")
+print(f"nelec = {nelec}")
+
+# Get CCSD t2 amplitudes for initializing the ansatz
+ccsd = pyscf.cc.CCSD(
+    scf, frozen=[i for i in range(mol.nao_nr()) if i not in active_space]
+).run()
+
+# Use 2 ansatz layers
+n_reps = 2
+# Use interactions implementable on a square lattice
+pairs_aa = [(p, p + 1) for p in range(norb - 1)]
+pairs_ab = [(p, p) for p in range(norb)]
+ucj_op = ffsim.UCJOpSpinBalanced.from_t_amplitudes(
+    ccsd.t2, n_reps=n_reps, interaction_pairs=(pairs_aa, pairs_ab)
+)
+
+# Construct circuit
+qubits = QuantumRegister(2 * norb)
+circuit = QuantumCircuit(qubits)
+circuit.append(ffsim.qiskit.PrepareHartreeFockJW(norb, nelec), qubits)
+circuit.append(ffsim.qiskit.UCJOpSpinBalancedJW(ucj_op), qubits)
+circuit.measure_all()
+qc = circuit
+
 # state_vector_actual = Statevector(qc)
-backend = FakeNairobi()
+# qc = random_circuit(20, 8)
+backend = FakeKyoto()
 coupling_map = backend.configuration().coupling_map
 # print(coupling_map)
 pass_manager = generate_preset_pass_manager(basis_gates=['ecr', 'id', 'rz', 'sx', 'x'], backend=backend,optimization_level=3)
 
 
-
-
-
-
 pass_manager_custom = PassManager()
 pass_manager_custom.append(Collect_3q_blocks())
 pass_manager_custom.append(ConsolidateBlocks())
-pass_manager_custom.append(MLUnitarySynthesis(coupling_map=coupling_map))
+pass_manager_custom_2 = PassManager()
+pass_manager_custom_2.append(MLUnitarySynthesis(coupling_map=coupling_map))
+pass_manager_custom_2.append(CommutationAnalysis())
+pass_manager_custom_2.append(CommutativeCancellation())
+pass_manager_custom_2.append(Optimize1qGates())
+
 
 transpiled_qc = pass_manager.run(qc)
+counts = QasmSimulator().run(transpiled_qc).result().get_counts()
+qiskit.visualization.plot_histogram(counts)
 
 print(f'Depth: {transpiled_qc.depth()}')
 
 # print(transpiled_qc.draw(idle_wires=False))
 
 
-transpiled_qc = pass_manager_custom.run(transpiled_qc).decompose()
-state_vector_actual = Statevector(transpiled_qc)
+transpiled_qc = pass_manager_custom.run(transpiled_qc)
+# state_vector_actual = Statevector(transpiled_qc)
 # print(transpiled_qc.draw(idle_wires=False))
+
+transpiled_qc = pass_manager_custom_2.run(transpiled_qc).decompose()
 print(f'Depth: {transpiled_qc.depth()}')
 
-state_vector_transpiled = Statevector(transpiled_qc)
+transpiled_qc = pass_manager_custom.run(transpiled_qc)
+transpiled_qc = pass_manager_custom_2.run(transpiled_qc).decompose()
+print(f'Depth: {transpiled_qc.depth()}')
 
-print(f'fidelity: {state_vector_actual.inner(state_vector_transpiled)}')
+# state_vector_transpiled = Statevector(transpiled_qc)
+
+# print(f'fidelity: {state_vector_actual.inner(state_vector_transpiled)}')
 
 
